7f0756a547fce47fe568fa689f4d3fbb73fe819a
Abdurrahim <abdurrahim22code@gmail.com>
2024-03-19 11:22:47 +0800

    1.Pendahuluan

---
+++ 1.Pendahuluan/Pendahuluan.java
@@ -0,0 +1,16 @@
+package pkg1.pendahuluan;
+
+public class Pendahuluan {
+
+    public static void main(String[] args) {
+        System.out.println("DATA DIRI\n");
+        
+        System.out.println("Nama Lengkap\t : Abdurrahim");
+        System.out.println("NPM\t\t : 2210010460");
+        System.out.println("Kelas\t\t : 4D REG BJB");
+        System.out.println("Prodi\t\t : Teknik Informatika");
+        System.out.println("Fakultas\t : Teknologi Informasi");
+        System.out.println("Status\t\t : Aktif");
+    }
+    
+}
